b347a9aa2c1e238ac4a54df15ae809d32d79b1ad
marians <marian@giantswarm.io>
2016-01-15 10:44:56 +0100

    Dummy change

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import "github.com/spf13/viper"
 // This is where no magic happens
 func main() {
 	// setting our default output message
-	viper.SetDefault("Message", "Hello world!")
+	viper.SetDefault("Message", "Hello world, you rock!")
 	// printing that message
 	fmt.Println(viper.Get("Message"))
 }
